0091c2a6a9792c71f0b327e9a43ddea0e9b459fe
Pedro Pombal Viegas Costa <pedro.costa@equilibrium.co.ao>
2022-04-19 17:02:38 +0100

    Added vacination store simple logic

--- app/Controllers/Http/VaccinationsController.ts
+++ app/Controllers/Http/VaccinationsController.ts
@@ -1,4 +1,5 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Person from 'App/Models/Person'
 import Vaccination from 'App/Models/Vaccination'
 import VaccinationValidator from 'App/Validators/VaccinationValidator'
 
@@ -8,11 +9,32 @@ export default class VaccinationsController {
     const vaccinationData = await request.validate(VaccinationValidator)
 
     try {
+      const person = await Person.find(vaccinationData.personId)
+
+      if (!person) {
+        return response.status(200).send({
+          message: 'Registro individual não encontrado!',
+          code: '200',
+          data: [],
+        })
+      }
+
       const vaccination = await Vaccination.create(vaccinationData)
 
-      return vaccination
+      await vaccination.load('person')
+
+      return response.status(201).send({
+        message: 'Utente vacinado com sucesso!',
+        code: 201,
+        data: { vaccination: vaccination },
+      })
     } catch (error) {
       console.log(error)
+      return response.status(500).send({
+        message: 'Ocorreu um erro no servidor!',
+        code: '500',
+        data: [],
+      })
     }
   }
 }
