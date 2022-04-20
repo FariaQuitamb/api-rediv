282c19c144a9876a7cff073bc2d98ecd349e9593
Pedro Pombal Viegas Costa <pedro.costa@equilibrium.co.ao>
2022-04-20 13:03:33 +0100

    Adding vaccination logic

--- app/Controllers/Http/VaccinationsController.ts
+++ app/Controllers/Http/VaccinationsController.ts
@@ -1,6 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Person from 'App/Models/Person'
 import Vaccination from 'App/Models/Vaccination'
+import Vaccine from 'App/Models/Vaccine'
 import VaccinationValidator from 'App/Validators/VaccinationValidator'
 
 export default class VaccinationsController {
@@ -11,6 +12,7 @@ export default class VaccinationsController {
     try {
       const person = await Person.find(vaccinationData.personId)
 
+      //RN [001]
       if (!person) {
         return response.status(200).send({
           message: 'Registro individual não encontrado!',
@@ -18,6 +20,100 @@ export default class VaccinationsController {
           data: [],
         })
       }
+
+      //RN [001]
+      if (person.status !== 'C') {
+        console.log('O status do registro individual é diferente de C')
+        return response.status(200).send({
+          message: 'O registro individual não reune condições para vacinação!',
+          code: '200',
+          data: [],
+        })
+      }
+
+      //Verifica se a vacina existe
+      //E se a dose selecionada pertence a vacina pré-selecionada
+
+      const vaccineDose = await Vaccine.query()
+        .preload('doses', (queryDose) => {
+          queryDose.where('Id_DoseVacina', vaccinationData.doseId)
+        })
+        .where('Id_Vacina', vaccinationData.vaccineId)
+        .first()
+
+      //Verifica se a vacina selecionada existe
+      if (vaccineDose) {
+        //Verifica se a dose selecionada pertence a vacina pré-selecionada
+        if (vaccineDose.doses.length === 0) {
+          console.log('A dose selecionada não pertence a essa vacina!')
+          return response.status(200).send({
+            message: 'A dose selecionada não pertence a essa vacina!',
+            code: '200',
+            data: [],
+          })
+        }
+      } else {
+        console.log('A vacina selecionada não existe!')
+        return response.status(200).send({
+          message: 'A vacina selecionada não existe!',
+          code: '200',
+          data: [],
+        })
+      }
+      /* Dose existe e pertence a vacina selecionada */
+
+      // Verifica se o utente já tomou essa dose da vacina
+
+      const selectedDose = vaccineDose.doses[0]
+
+      //Caso não esteja realizando dose de reforço
+      if (vaccinationData.status !== 'R') {
+        //
+        //Se for primeira dose ou dose única , não deve ter outra vacinafeita
+        if (selectedDose.name === '1ª Dose' || selectedDose.name === 'Dose Única') {
+          const personVaccinations = await Vaccination.query()
+            .preload('person')
+            //.preload('vaccine')
+            .where('Id_regIndividual', person.id)
+            .limit(1000)
+          //Verifica se já possuí vacina
+          if (personVaccinations.length > 0) {
+            console.log(`O utente já possuí vacina não pode adicionar a ${selectedDose.name}!`)
+            return response.status(200).send({
+              message: `O utente já possuí vacina não pode adicionar a ${selectedDose.name}!`,
+              code: '200',
+              data: [],
+            })
+          }
+        }
+
+        //Caso esteja registrando a segunda dose
+        if (selectedDose.name === '2ª Dose') {
+          const personVaccinations = await Vaccination.query()
+            .preload('person')
+            .preload('vaccine')
+            .preload('dose')
+            .where('Id_regIndividual', person.id)
+            //.where('Id_Dose', vaccinationData.doseId)
+            .limit(1000)
+
+          return { segundaDose: personVaccinations }
+        }
+      }
+
+      if (selectedDose.name !== 'Dose Única') {
+        const personVaccinations = await Vaccination.query()
+          .preload('person')
+          .preload('vaccine')
+          .preload('dose')
+          .where('Id_regIndividual', person.id)
+          .where('Id_Dose', vaccinationData.doseId)
+          .limit(1000)
+
+        return personVaccinations
+      }
+
+      return 'Dose Única'
 
       const vaccination = await Vaccination.create(vaccinationData)
 
