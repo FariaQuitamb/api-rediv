214249bfa9b8176fecfb3cd390ad9b132f71f583
Pedro Pombal Viegas Costa <pedro.costa@equilibrium.co.ao>
2022-04-21 16:43:48 +0100

    Added constant for sql string avoid code mess

---
+++ contracts/constants/constants.ts
@@ -0,0 +1,32 @@
+let sql = ''
+sql = 'Select Top(1) [Id_regVacinacao]'
+sql += ',[Id_Dose]'
+sql += ',[NumDias]'
+sql += ',[PrxDose]'
+sql += ',[Id_Vacina]'
+sql += ',CONVERT(VARCHAR, DATEADD(day, [NumDias], [DataCad]), 23) AS [DtProxDose]'
+sql += ',CONVERT(VARCHAR, [DataCad], 23) As [DataCad]'
+sql += ',DATEDIFF(day, DATEADD(day, [NumDias], [DataCad]),  getdate()) AS [NumDiasRestante]'
+sql += ',DATEDIFF(day, getdate(), DATEADD(day, [NumDias], [DataCad])) AS [NumDias2]'
+sql += ',CONVERT(VARCHAR, GETDATE(), 23) AS [dtHoje] '
+sql += 'From ('
+sql +=
+  'SELECT ROW_NUMBER() OVER(PARTITION BY rv.[Id_Dose] ORDER BY rv.[DataCad] DESC) AS [NumLinha]'
+sql += ',rv.[Id_regVacinacao]'
+sql += ',rv.[Id_Vacina]'
+sql += ',rv.[Id_Dose]'
+sql +=
+  ",[NumDias] = (Isnull((Select [NumDias] From [vac_DoseVacina] Where [Visualizar] = 'S' And [Id_Vacina] = rv.[Id_Vacina] And [NumOrdem] = (dv.[NumOrdem] + 1)), 0))"
+sql +=
+  ",[PrxDose] = (Isnull((Select [Id_DoseVacina] From [vac_DoseVacina] Where [Visualizar] = 'S' And [Id_Vacina] = rv.[Id_Vacina] And [NumOrdem] = (dv.[NumOrdem] + 1)), 0))"
+sql += ',rv.[DataCad] '
+sql += 'FROM [dbo].[vac_regVacinacao] rv '
+sql += 'Inner Join [vac_DoseVacina] dv On dv.[Id_DoseVacina] = rv.[Id_Dose] '
+sql += "Where rv.[Status] <> 'R' And rv.[Id_regIndividual] = ?"
+sql += ') As Tbl '
+sql += 'Order By [DataCad] Desc'
+
+const constants = {
+  sqlFirstSecondDose: sql,
+}
+export default constants
