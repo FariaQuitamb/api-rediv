ca37a3b819e47bf08b55c4d7cf7f6a2e37e37707
Pedro Pombal Viegas Costa <pedro.costa@equilibrium.co.ao>
2022-04-27 16:03:07 +0100

    work type data

--- app/Models/PostWorkerType.ts
+++ app/Models/PostWorkerType.ts
@@ -1,5 +1,4 @@
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
-
 export default class PostWorkerType extends BaseModel {
   public static table = 'vac_tipoFuncPostoVac'
   @column({ isPrimary: true, columnName: 'Id_tipoFuncPostoVac' })
