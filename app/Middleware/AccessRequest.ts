7f67b31a4e215921cf7d035a34a15bf8e5acf04f
EdroViegas <pedropombalviegas@gmail.com>
2022-06-08 16:34:02 +0100

    Implemented access middleware logic and strategy , working well

--- app/Middleware/AccessRequest.ts
+++ app/Middleware/AccessRequest.ts
@@ -1,15 +1,26 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import ApiAccess from 'App/Models/ApiAccess'
+import accessInfo from 'Contracts/functions/access_info'
 
 export default class AccessRequest {
   public async handle({ request, response }: HttpContextContract, next: () => Promise<void>) {
     // code for middleware goes here. ABOVE THE NEXT CALL
 
-    console.log(`-> ${request.method()}: ${request.url()}`)
+    const accessHeader = accessInfo(request.headers())
 
-    const notAuthenticated = false
+    if (accessHeader.id === 'undefined' || accessHeader.accessKey === 'undefined') {
+      response.unauthorized({ code: 401, message: 'Envie os dados de acesso correctamente!' })
+      return
+    }
 
-    if (notAuthenticated) {
-      response.unauthorized({ error: 'Must be logged in' })
+    const access = await ApiAccess.query()
+      .where('api_id', accessHeader.id)
+      .where('access_key', accessHeader.accessKey)
+      .where('state', 1)
+      .first()
+
+    if (!access) {
+      response.unauthorized({ code: 401, message: 'Acesso negado!' })
       return
     }
 
