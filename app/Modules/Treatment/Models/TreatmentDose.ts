985d3d8c524d2367f35801db676d33a9f323ea9d
EdroViegas <pedropombalviegas@gmail.com>
2022-07-05 16:57:48 +0100

    New models for Treatment context

---
+++ app/Modules/Treatment/Models/TreatmentDose.ts
@@ -0,0 +1,18 @@
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class TreatmentDose extends BaseModel {
+  @column({ isPrimary: true, columnName: 'Id_tratDose' })
+  public id: number
+  @column({ columnName: 'Id_tipoDoenca' })
+  public illnessId: number
+  @column({ columnName: 'Id_Tratamento' })
+  public treatmentId: number
+  @column({ columnName: 'Nome' })
+  public name: string
+  @column({ columnName: 'NumDias' })
+  public numDays: number
+  @column({ columnName: 'NumOrdem' })
+  public orderNumber: number
+  @column({ columnName: 'Visualizar' })
+  public visualize: string
+}
