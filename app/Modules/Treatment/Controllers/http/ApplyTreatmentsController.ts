fd76208af74d8c58f10ae034584f7cb0e5d8e3ed
EdroViegas <pedropombalviegas@gmail.com>
2022-07-07 12:53:35 +0100

    Changing treatment insert to multi insert

--- app/Modules/Treatment/Controllers/http/ApplyTreatmentsController.ts
+++ app/Modules/Treatment/Controllers/http/ApplyTreatmentsController.ts
@@ -11,72 +11,20 @@ import formatHeaderInfo from 'Contracts/functions/format_header_info'
 import formatUserInfo from 'Contracts/functions/format_user_info'
 import formatError from 'Contracts/functions/format_error'
 import logError from 'Contracts/functions/log_error'
+import resolveTreatment from 'Contracts/Treatment/functions/resolve_treatments'
 
 export default class ApplyTreatmentsController {
   public async index({}: HttpContextContract) {}
 
   public async store({ auth, response, request }: HttpContextContract) {
-    const treatmentData = await request.validate(ApplyTreatmentValidator)
+    const treatmentsData = await request.validate(ApplyTreatmentValidator)
+
     try {
-      if (isAfterToday(treatmentData.createdAt)) {
-        const previewsDate = treatmentData.createdAt
-        treatmentData.createdAt = moment().toISOString()
-        formatedLog({
-          text: `A data de aplicação do tratamento foi modificada para data de hoje! Data Inserida: ${previewsDate} User: Id:${auth.user?.id} Name: ${auth.user?.name} Phone: ${auth.user?.phone} BI:${auth.user?.bi}`,
-          data: treatmentData,
-          auth: auth,
-          request: request,
-          type: LogType.warning,
-        })
-      }
+      const treatments = resolveTreatment(request, auth, treatmentsData.treatments)
 
-      const prevDate = treatmentData.createdAt
-      treatmentData.createdAt = moment(treatmentData.createdAt, moment.ISO_8601, true).toISOString()
-      //Caso tenha inserido data que não seja possível converter
-      if (treatmentData.createdAt === null) {
-        treatmentData.createdAt = moment().toISOString()
-        formatedLog({
-          text: `A data de aplicação do tratamento foi modificada para data de hoje! Data Inserida: ${prevDate} User: Id:${auth.user?.id} Name: ${auth.user?.name} Phone: ${auth.user?.phone} BI:${auth.user?.bi}`,
-          data: treatmentData,
-          auth: auth,
-          request: request,
-          type: LogType.warning,
-        })
-      }
-
-      treatmentData.treatmentDoseId = 1001
-
-      const appliedTreatment = await AppliedTreatment.create(treatmentData)
-
-      const version = Env.get('API_VERSION')
-
-      await logRegister({
-        id: auth.user?.id ?? 0,
-        system: 'MB',
-        screen: 'ApplyTreatmentsController/store',
-        table: 'vac_vacTratamento',
-        job: 'Cadastrar',
-        tableId: appliedTreatment.id,
-        action: 'Aplicação de tratamento',
-        actionId: `V:${version}-ID:${appliedTreatment.id.toString()}`,
-      })
-
-      formatedLog({
-        text: 'Tratamento aplicado com sucesso!',
-        type: LogType.success,
-        data: appliedTreatment,
-        auth: auth,
-        request: request,
-      })
-
-      //Utente registado com sucesso
-      return response.status(HttpStatusCode.CREATED).send({
-        message: 'Tratamento aplicado com sucesso',
-        code: HttpStatusCode.CREATED,
-        data: appliedTreatment,
-      })
+      return treatments
     } catch (error) {
-      const treatmentStr = JSON.stringify(treatmentData)
+      const treatmentStr = JSON.stringify(treatmentsData)
 
       const deviceInfo = JSON.stringify(formatHeaderInfo(request))
       const userInfo = formatUserInfo(auth.user)
@@ -95,7 +43,7 @@ export default class ApplyTreatmentsController {
         formatedLog({
           text: 'Não foi possível realizar a operação de registo de tratamento dentro do tempo esperado!',
           type: LogType.warning,
-          data: treatmentData,
+          data: treatmentsData,
           auth: auth,
           request: request,
         })
