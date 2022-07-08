2b49de7d5d94de0acb3959c8ceea93343093feb9
EdroViegas <pedropombalviegas@gmail.com>
2022-07-08 23:31:59 +0100

    Added get date by months or years old

--- app/Modules/Treatment/Validators/ChildValidator.ts
+++ app/Modules/Treatment/Validators/ChildValidator.ts
@@ -47,7 +47,7 @@ export default class ChildValidator {
    * for targeting nested fields and array expressions `(*)` for targeting all
    * children of an array. For example:
    *
-   * {
+   *
    *   'profile.username.required': 'Username is required',
    *   'scores.*.number': 'Define scores as valid numbers'
    * }
