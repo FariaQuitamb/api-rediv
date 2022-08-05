438222c2c9004cf4eed6a071b2afb8f7ab66660c
EdroViegas <pedropombalviegas@gmail.com>
2022-08-05 17:14:09 +0100

    Added new method to get treatments works from user

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -15,16 +15,6 @@ export default class UsersController {
   public async userWork({ auth, request, response }: HttpContextContract) {
     const searchData = await request.validate(UserWorkValidator)
     try {
-      /*
-
- 
- 
-
-  
-
-
-     */
-
       const fields: Array<{ field: string; value: any }> = [
         {
           field: ' [up].[Id_userPostoVacinacao]',
@@ -62,7 +52,7 @@ export default class UsersController {
         .select(constants.userWorkFields)
         .joinRaw(constants.userWorkSources)
         .whereRaw(query)
-        .orderBy('Data', 'desc')
+        .orderBy('[vac].[DataCad]', 'desc')
         .paginate(searchData.page, searchData.limit)
 
       return response.status(HttpStatusCode.OK).send({
@@ -77,14 +67,81 @@ export default class UsersController {
       const errorInfo = formatError(error)
       await logError({
         type: 'MB',
-        page: 'AuthController/aboutUsers',
+        page: 'UsersController/userWork',
         error: `User: ${userInfo} Device: ${deviceInfo} ${errorInfo} `,
         request: request,
       })
       return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send({
         code: HttpStatusCode.INTERNAL_SERVER_ERROR,
         details: error,
-        message: 'Não foi possível obter informação sobre os utilizadores',
+        message: 'Não foi possível obter informação sobre o trabalho dos utilizadores',
+      })
+    }
+  }
+
+  public async userWorkTreatment({ auth, request, response }: HttpContextContract) {
+    const searchData = await request.validate(UserWorkValidator)
+    try {
+      const fields: Array<{ field: string; value: any }> = [
+        {
+          field: ' [up].[Id_userPostoVacinacao]',
+          value: searchData.userId,
+        },
+        { field: '[up].[Nome]', value: searchData.personalName },
+        {
+          field: '[up].[Utilizador]',
+          value: searchData.username,
+        },
+        { field: '[BI]', value: searchData.nationalID },
+        { field: '[Telefone]', value: searchData.phone },
+        { field: '[fn].[Nome]', value: searchData.role },
+        { field: '[TipoPosto] ', value: searchData.postType },
+
+        { field: '[NomeEM] ', value: searchData.postName },
+        { field: '[NomeResp] ', value: searchData.postManagerName },
+        { field: '[BIResp] ', value: searchData.postManagerNationalId },
+        { field: '[TelResp]', value: searchData.postManagerPhone },
+        { field: '[p].[Nome]', value: searchData.province },
+        { field: '[mun].[Nome]', value: searchData.municipality },
+        { field: '[Mac_address]', value: searchData.deviceId },
+
+        { field: '[SIGIS].[dbo].[vac_regVacinacaoLog].[Latitude] ', value: searchData.latitude },
+        { field: '[SIGIS].[dbo].[vac_regVacinacaoLog].[Longitude]', value: searchData.longitude },
+
+        { field: '[Tipo]', value: searchData.vaccineDose },
+        { field: '[vac].[DataCad]', value: searchData.vaccinationDate },
+      ]
+
+      let query = generateQuery(fields)
+
+      //Logged in
+      const userWork = await Database.from(constants.userWorkTreatmentTable)
+        .select(constants.userWorkTreatmentFields)
+        .joinRaw(constants.userWorkTreatmentSources)
+        //.whereRaw(query)
+        //.orderBy('[vac].[DataCad]', 'desc')
+        .paginate(searchData.page, searchData.limit)
+
+      return response.status(HttpStatusCode.OK).send({
+        message: 'Trabalho realizado: filtro =' + query,
+        code: HttpStatusCode.OK,
+        data: { userWork },
+      })
+    } catch (error) {
+      //Log de erro
+      const deviceInfo = JSON.stringify(formatHeaderInfo(request))
+      const userInfo = formatUserInfo(auth.user)
+      const errorInfo = formatError(error)
+      await logError({
+        type: 'MB',
+        page: 'UsersController/userWork',
+        error: `User: ${userInfo} Device: ${deviceInfo} ${errorInfo} `,
+        request: request,
+      })
+      return response.status(HttpStatusCode.INTERNAL_SERVER_ERROR).send({
+        code: HttpStatusCode.INTERNAL_SERVER_ERROR,
+        details: error,
+        message: 'Não foi possível obter informação sobre o trabalho dos utilizadores',
       })
     }
   }
