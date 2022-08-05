13d4f6d75c8e348f93c926a842f96a803447868c
EdroViegas <pedropombalviegas@gmail.com>
2022-08-05 12:51:49 +0100

    Added new method to get user work

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -27,17 +27,17 @@ export default class UsersController {
 
       const fields: Array<{ field: string; value: any }> = [
         {
-          field: ' [SIGIS].[dbo].[vac_userPostoVacinacao].[Id_userPostoVacinacao]',
+          field: ' [up].[Id_userPostoVacinacao]',
           value: searchData.userId,
         },
-        { field: '[SIGIS].[dbo].[vac_userPostoVacinacao].[Nome]', value: searchData.personalName },
+        { field: '[up].[Nome]', value: searchData.personalName },
         {
-          field: '[SIGIS].[dbo].[vac_userPostoVacinacao].[Utilizador]',
+          field: '[up].[Utilizador]',
           value: searchData.username,
         },
         { field: '[BI]', value: searchData.nationalID },
         { field: '[Telefone]', value: searchData.phone },
-        { field: '[SIGIS].[dbo].[vac_tipoFuncPostoVac].[Nome]', value: searchData.role },
+        { field: '[fn].[Nome]', value: searchData.role },
         { field: '[TipoPosto] ', value: searchData.postType },
 
         { field: '[NomeEM] ', value: searchData.postName },
@@ -45,18 +45,17 @@ export default class UsersController {
         { field: '[BIResp] ', value: searchData.postManagerNationalId },
         { field: '[TelResp]', value: searchData.postManagerPhone },
         { field: '[p].[Nome]', value: searchData.province },
-        { field: '[SIGIS].[dbo].[Municipio].[Nome]', value: searchData.municipality },
+        { field: '[mun].[Nome]', value: searchData.municipality },
         { field: '[Mac_address]', value: searchData.deviceId },
 
         { field: '[SIGIS].[dbo].[vac_regVacinacaoLog].[Latitude] ', value: searchData.latitude },
         { field: '[SIGIS].[dbo].[vac_regVacinacaoLog].[Longitude]', value: searchData.longitude },
 
         { field: '[Tipo]', value: searchData.vaccineDose },
-        { field: 'vac.DataCad', value: searchData.vaccinationDate },
+        { field: '[vac].[DataCad]', value: searchData.vaccinationDate },
       ]
 
       let query = generateQuery(fields)
-      console.log(query)
 
       //Logged in
       const userWork = await Database.from(constants.userWorkTable)
